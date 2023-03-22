40081f93244229322464971893971abd4228ede4
Mikael Auno <mikael.auno@viaplaygroup.com>
2023-03-22 11:45:50 +0100

    Add key bindings and completions for fzf

---
+++ zshrc.d/90-fzf.zsh
@@ -0,0 +1,6 @@
+# vim: set ts=4 sw=4 expandtab:
+#
+if type fzf > /dev/null; then
+  [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
+  [ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
+fi
