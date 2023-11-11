2ca9e678638d69234ef4348a26d47dc207282a48
Mikael Auno <mikael@auno.se>
2023-11-11 19:32:53 +0100

    Allow for name `batcat` in configuration of `bat`

--- zshrc.d/20-misc-path-dependent.zsh
+++
@@ -1,6 +0,0 @@
-# vim: set ts=2 sw=2 expandtab:
-
-if type bat > /dev/null; then
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
-fi
-
---
+++ zshrc.d/90-bat.zsh
@@ -0,0 +1,15 @@
+# vim: set ts=2 sw=2 expandtab:
+
+BAT=bat
+
+if ! type $BAT > /dev/null; then
+  if type batcat > /dev/null; then
+    alias bat=batcat
+    BAT=batcat
+  fi
+fi
+
+if type $BAT > /dev/null; then
+  export MANPAGER="sh -c 'col -bx | $BAT -l man -p'"
+fi
+
