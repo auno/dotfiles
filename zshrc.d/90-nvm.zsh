06f72627d02b6ba75415fe6fd3ddf2563073c267
Mikael Auno <mikael@auno.se>
2025-04-06 13:46:32 +0200

    Always export NVM_DIR

--- zshrc.d/90-nvm.zsh
+++ zshrc.d/90-nvm.zsh
@@ -1,9 +1,9 @@
+export NVM_DIR="$HOME/.nvm"
+
 # Lazy load NVM
 if [[ "$UID" != "0" && -d "$HOME/.nvm" ]]; then
   nvm() {
     unset -f nvm
-
-    export NVM_DIR="$HOME/.nvm"
 
     [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
     if [ -d "$HOME/.npm-packages/bin" ]; then
