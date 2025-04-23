4fa9249086918b62a13ccbf53a1fdc2105b45178
Mikael Auno <mikael@auno.se>
2025-04-23 21:00:06 +0200

    Add completions for fd

--- zshrc.d/85-fd.zsh
+++ zshrc.d/85-fd.zsh
@@ -5,3 +5,13 @@ if ! type fd > /dev/null; then
     alias fd=fdfind
   fi
 fi
+
+if type fd > /dev/null; then
+  _fd() {
+    unset -f _fd
+    . <(fd --gen-completions zsh)
+    _fd "$@"
+  }
+
+  compdef _fd fd
+fi
