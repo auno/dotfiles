004e89f920be44cd84c469e2b72397187124cf5f
Mikael Auno <mikael@auno.se>
2025-04-23 20:39:41 +0200

    Add completions for rustup and cargo

--- zshrc.d/11-cargo.zsh
+++ zshrc.d/11-cargo.zsh
@@ -1,3 +1,19 @@
 # vim: set ts=2 sw=2 expandtab:
 
 zshrc::add_to_path "$HOME/.cargo/bin"
+
+_rustup() {
+  unset -f _rustup
+  . <(rustup completions zsh)
+  _rustup "$@"
+}
+
+compdef _rustup rustup
+
+_cargo() {
+  unset -f _cargo
+  . <(rustup completions zsh cargo)
+  _cargo "$@"
+}
+
+compdef _cargo cargo
