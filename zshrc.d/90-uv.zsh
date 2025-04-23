6312fa2ef5219ff2c1fb62d62b07a61e36626e5c
Mikael Auno <mikael@auno.se>
2025-04-23 13:25:00 +0200

    Add completions for uv

---
+++ zshrc.d/90-uv.zsh
@@ -0,0 +1,23 @@
+if type uv > /dev/null; then
+  ___load_uv_completions() {
+    unset -f _uv
+    unset -f _uvx
+    unset -f ___load_uv_completions
+
+    . <(uv generate-shell-completion zsh)
+    . <(uvx --generate-shell-completion zsh)
+  }
+
+  _uv() {
+    ___load_uv_completions
+    _uv "$@"
+  }
+
+  _uvx() {
+    ___load_uv_completions
+    _uvx "$@"
+  }
+
+  compdef _uv uv
+  compdef _uvx uvx
+fi
