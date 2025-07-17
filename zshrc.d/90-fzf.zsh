86c613a08788dc29094c917f6b53cb3ba99a61e8
Mikael Auno <mikael.auno@viaplaygroup.com>
2025-07-17 16:12:58 +0200

    Add fzf man page selection when calling man without arguments

--- zshrc.d/90-fzf.zsh
+++ zshrc.d/90-fzf.zsh
@@ -18,4 +18,21 @@ if type fzf > /dev/null; then
 
   [ -f /usr/share/doc/fzf/examples/key-bindings.zsh ] && source /usr/share/doc/fzf/examples/key-bindings.zsh
   [ -f /usr/share/doc/fzf/examples/completion.zsh ] && source /usr/share/doc/fzf/examples/completion.zsh
+
+  man-fzf() {
+    if [[ $# == 0 ]]; then
+      args=$(
+        man -k . \
+          | fzf -q "$1" --prompt='man> '  --preview $'echo {} | tr -d \'()\' | awk \'{printf "%s ", $2} {print $1}\' | xargs -r man | col -bx | bat -l man -p --color always' \
+          | tr -d '()' \
+          | awk '{printf "%s ", $2} {print $1}'
+      )
+      test -n "$args" || return
+      "man" "${=args}"
+    else
+      "man" "$@"
+    fi
+  }
+
+  alias man=man-fzf
 fi
