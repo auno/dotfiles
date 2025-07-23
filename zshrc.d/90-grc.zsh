a18bd16ba1a8208352dd40d1a6838ece0362dca3
Mikael Auno <mikael@auno.se>
2025-07-23 23:38:11 +0200

    Use provided loading for grc

--- zshrc.d/90-grc.zsh
+++ zshrc.d/90-grc.zsh
@@ -1,16 +1,6 @@
 # vim: set ts=2 sw=2 expandtab:
 
 # Automatically enable grc for all recognized commands if available
-if type grc > /dev/null; then
-  GRC_DIRS=("~/.grc/" "/usr/share/grc/" "/usr/local/share/grc/")
-
-  for cmd in $(ls -1 $GRC_DIRS 2>/dev/null | grep '^conf\.' | sed -r 's/^conf\.//' | sort -u); do
-    if alias "$cmd" > /dev/null; then
-      if [[ "$(whence "$cmd")" == "$cmd "* ]]; then
-        alias "$cmd"="grc $(whence "$cmd")"
-      fi
-    elif type -p "$cmd" > /dev/null; then
-      alias "$cmd"="grc $cmd"
-    fi
-  done
+if type grc > /dev/null && [[ -f "/etc/grc.zsh" ]]; then
+  . /etc/grc.zsh
 fi
