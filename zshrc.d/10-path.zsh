5c177ba52b89f565e6a4199aae0355becc5f7f68
Mikael Auno <mikael@auno.se>
2025-07-24 19:56:30 +0200

    Add support to `path` for editing other CDPATH and FPATH

--- zshrc.d/10-path.zsh
+++ zshrc.d/10-path.zsh
@@ -17,38 +17,70 @@ path() {
   fi
 
   local usage_text=(
-    "Usage: path [command]"
+    "Usage: [option...] path [command]"
     ""
     "Commands:"
     ""
     "  list      list path elements"
     "  ls        alias for list"
     "  show      alias for list"
+    ""
+    "Options:"
+    ""
+    "  --cdpath  edit \$CDPATH instead of \$PATH"
+    "  --fpath   edit \$FPATH instead of \$PATH"
   )
 
-  case "$1" in
-    list | ls | show | "")
-      echo "$PATH" \
-        | tr ':' '\n' \
-        | $list_formatter
-      ;;
-    edit)
-      ___zsh_path_edit_temp_file=$(mktemp "${TMPDIR:-/tmp}/zsh-path-edit.XXXXXX")
-      echo "$PATH" | tr ':' '\n' >! "$___zsh_path_edit_temp_file"
-      "${EDITOR:-vim}" "$___zsh_path_edit_temp_file" || return 1
-      path=("${(f)$(cat "$___zsh_path_edit_temp_file")}")
-      ;;
-    help | --help)
-      (IFS=$'\n'; echo "${usage_text[*]}") \
-        | $usage_formatter
-      ;;
-    *)
-      echo "Error: Unknown command: $1" >&2
-      echo >&2
-      (IFS=$'\n'; echo "${usage_text[*]}") \
-        | $usage_formatter \
-        >&2
-      return 1
-      ;;
-  esac
+  local variable_name="PATH"
+
+  while true; do
+    case "$1" in
+      "list" | "ls" | "show" | "")
+        echo "${(P)variable_name}" \
+          | tr ':' '\n' \
+          | $list_formatter
+        break
+        ;;
+      "edit")
+        local variable_array_name="${(L)variable_name}"
+        ___zsh_path_edit_temp_file=$(mktemp "${TMPDIR:-/tmp}/zsh-path-edit.XXXXXX")
+        echo "${(P)variable_name}" | tr ':' '\n' >! "$___zsh_path_edit_temp_file"
+        "${EDITOR:-vim}" "$___zsh_path_edit_temp_file" || return 1
+        local -a updated_paths=("${(f)$(cat "$___zsh_path_edit_temp_file")}")
+        case "$variable_name" in
+          "PATH")
+            path=($updated_paths)
+            ;;
+          "CDPATH")
+            cdpath=($updated_paths)
+            ;;
+          "FPATH")
+            fpath=($updated_paths)
+            ;;
+        esac
+        break
+        ;;
+      "help" | "--help")
+        (IFS=$'\n'; echo "${usage_text[*]}") \
+          | $usage_formatter
+        break
+        ;;
+      "--cdpath")
+        variable_name="CDPATH"
+        shift
+        ;;
+      "--fpath")
+        variable_name="FPATH"
+        shift
+        ;;
+      *)
+        echo "Error: Unknown command: $1" >&2
+        echo >&2
+        (IFS=$'\n'; echo "${usage_text[*]}") \
+          | $usage_formatter \
+          >&2
+        return 1
+        ;;
+    esac
+  done
 }
