91c67403f63a9353fd5707c1f3716a235d313fed
Mikael Auno <mikael@auno.se>
2026-01-22 14:54:14 +0100

    mise: Add exception for make when loading grc

--- zsh/zshrc.d/90-grc.zsh
+++ zsh/zshrc.d/90-grc.zsh
@@ -2,5 +2,27 @@
 
 # Automatically enable grc for all recognized commands if available
 if type grc > /dev/null && [[ -f "/etc/grc.zsh" ]]; then
+  ignored_cmds=(
+    gmake
+    make
+  )
+
+  for ignored_cmd in $ignored_cmds; do
+    if (( $+functions[$ignored_cmd] )); then
+      functions -c $ignored_cmd $ignored_cmd.pre-grc-backup
+    fi
+  done
+
   . /etc/grc.zsh
+
+  for ignored_cmd in $ignored_cmds; do
+    if (( $+functions[$ignored_cmd.pre-grc-backup] )); then
+      functions -c $ignored_cmd.pre-grc-backup $ignored_cmd
+      unfunction $ignored_cmd.pre-grc-backup
+    else
+      unfunction $ignored_cmd
+    fi
+  done
+
+  unset ignored_cmds ignored_cmd
 fi
